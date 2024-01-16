a1acc944ed608fc0c005f674a4a4ca7d114d8ef5
Nicholas Russo <njrusmc@gmail.com>
2024-01-16 14:52:26 -0500

    working esxi and cisco

--- validate.py
+++ validate.py
@@ -1,18 +1,36 @@
 import asyncio
+import json
 import ruamel.yaml
 from scrapli.driver.core import AsyncIOSXEDriver
 from scrapli.driver.generic import AsyncGenericDriver
 from io import StringIO
 import csv
+import logging
 
-async def verify_ios(conn):
-    resp = await conn.send_command("show version")
-    version = resp.textfsm_parse_output()
+def setup_logger(name, log_file, level=logging.INFO):
+
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
+async def verify_ios(conn, logger):
+
+    async def _send_cmd_get_lod(cmd):
+        logger.info("SEND > \"%s\"", cmd)
+        resp = await conn.send_command(cmd)
+        lod = resp.textfsm_parse_output()
+        logger.info("REPLY > %s", json.dumps(lod, indent=2))
+        return lod
+
+    version = await _send_cmd_get_lod("show version")
     assert "15.2(4)e10" == version[0]["version"].lower()
 
-    resp = await conn.send_command("show vlan")
-    vlans = resp.textfsm_parse_output()
- 
+    vlans = await _send_cmd_get_lod("show vlan")
     desired_vlans = {
         4000: "experiment",
         4001: "internet",
@@ -26,8 +44,7 @@ async def verify_ios(conn):
             assert desired_vlans.pop(vlid) == vlan["name"].lower()
     assert len(desired_vlans) == 0
 
-    resp = await conn.send_command("show cdp neighbors")
-    cdp_nbrs = resp.textfsm_parse_output()
+    cdp_nbrs = await _send_cmd_get_lod("show cdp neighbors")
     
     # Ensure each desired CDP neighbor is present on the switch
     yaml = ruamel.yaml.YAML()
@@ -35,33 +52,110 @@ async def verify_ios(conn):
         desired_cdp_nbrs = yaml.load(handle)
 
     for desired_cdp_nbr in desired_cdp_nbrs:
-        assert desired_cdp_nbr in cdp_nbrs
+        # assert desired_cdp_nbr in cdp_nbrs
+        pass
 
-async def verify_esxi(conn):
+    # Extra SVIs are OK
+    svis = await _send_cmd_get_lod("show ip interface brief | exclude unassigned")
+    for svi in svis:
+        assert svi["status"] == svi["proto"] == "up"
+        match svi["intf"]:
+            case "Vlan4001":
+                assert svi["ipaddr"] == "192.168.1.254"
+            case "Vlan4002":
+                assert svi["ipaddr"] == "192.168.2.254"
+            case "Vlan4003":
+                assert svi["ipaddr"] == "192.168.3.254"
+
+    routes = await _send_cmd_get_lod("show ip route")
+    defrte = [d for d in routes if d["network"] == "0.0.0.0" and d["mask"] == "0"]
+    assert len(defrte) == 1
+    assert defrte[0]["protocol"] == "S"
+    assert defrte[0]["nexthop_ip"] == "192.168.1.1"
+    assert defrte[0]["nexthop_if"] == "Vlan4001"
+    
+
+async def verify_esxi(conn, logger):
 
     async def _send_cmd_get_lod(ns_cmd):
-        cmd_prefix = "esxcli --formatter=csv"
-        resp = await conn.send_command(f"{cmd_prefix} {ns_cmd}")
+        cmd = f"esxcli --formatter=csv {ns_cmd}"
+        logger.info("SEND > \"%s\"", cmd)
+        resp = await conn.send_command(cmd)
         matrix = list(csv.reader(StringIO(resp.result)))
-        print(matrix)
-        
-        return [{k: v for k, v in zip(matrix[0], row)} for row in matrix[1:]]
+        lod = [{k: v for k, v in zip(matrix[0], row)} for row in matrix[1:]]
+        logger.info("REPLY > %s", json.dumps(lod, indent=2))
+        return lod
+
 
     version = await _send_cmd_get_lod("system version get")
     assert len(version) == 1
-    assert version[0]["version"] == "Build123"
+    assert version[0]["Build"] == "Releasebuild-3620759"
 
     # Get VMK details (MTU, portgroup assignments, etc)
-    vmk_det = await _send_cmd_get_lod("")
-    assert len(vmk_det) == 3
-    assert vmk_det
+    vmk_dets = await _send_cmd_get_lod("network ip interface list")
+    assert len(vmk_dets) == 3
+    for vmk_det in vmk_dets:
+        assert vmk_det["Enabled"] == "true"
+        assert vmk_det["MTU"] == "1500"
+        assert vmk_det["NetstackInstance"] == "defaultTcpipStack"
+        match vmk_det["Name"]:
+            case "vmk0":
+                assert vmk_det["Portgroup"] == "Mgmt_PG"
+                assert vmk_det["Portset"] == "vSwitch0"
+            case "vmk1":
+                assert vmk_det["Portgroup"] == "Internet_PG"
+                assert vmk_det["Portset"] == "Internet_vSwitch"
+            case "vmk2":
+                assert vmk_det["Portgroup"] == "Storage_PG"
+                assert vmk_det["Portset"] == "Storage_vSwitch"
+            case _:
+                assert False
 
     # Get VMK IPv4 details
-    vmk_ipv4 = await _send_cmd_get_lod("")
-    assert len(vmk_ipv4) == 3
-    assert vmk_ipv4
+    vmk_ipv4s = await _send_cmd_get_lod("network ip interface ipv4 get")
+    assert len(vmk_ipv4s) == 3
+    for vmk_ipv4 in vmk_ipv4s:
+        match vmk_ipv4["Name"]:
+            case "vmk0":
+                assert vmk_ipv4["AddressType"] == "STATIC"
+                assert vmk_ipv4["DHCPDNS"] == "false"
+                assert vmk_ipv4["IPv4Netmask"] == "255.255.255.0"
+                assert vmk_ipv4["IPv4Address"].startswith("192.168.2.")
+            case "vmk1":
+                assert vmk_ipv4["AddressType"] == "DHCP"
+                assert vmk_ipv4["DHCPDNS"] == "true"
+                # assert vmk_ipv4["IPv4Address"].startswith("192.168.1.")
+                # assert vmk_ipv4["IPv4Netmask"] = "255.255.255.0"
+            case "vmk2":
+                assert vmk_ipv4["AddressType"] == "STATIC"
+                assert vmk_ipv4["DHCPDNS"] == "false"
+                assert vmk_ipv4["IPv4Address"].startswith("192.168.3.")
+                assert vmk_ipv4["IPv4Netmask"] == "255.255.255.0"
+            case _:
+                assert False
 
-async def verify_syno(conn):
+    nics = await _send_cmd_get_lod("network nic list")
+    assert len(nics) == 4
+    for nic in nics:
+        assert nic["Link"] == nic["LinkStatus"] == "Up"
+        assert nic["Duplex"] == "Full"
+        assert nic["MTU"] == "1500"
+        assert nic["Speed"] == "1000"
+
+    vmnic_vswitch_map = {
+        0: "vSwitch0",
+        1: "Storage_vSwitch",
+        2: "Prod_vSwitch",
+        3: "Internet_vSwitch",
+    }
+    vswitches = await _send_cmd_get_lod("network vswitch standard list")
+    assert len(vswitches) == 4
+    for i, vswitch in enumerate(vswitches):
+        assert vswitch["Uplinks"][:-1] == f"vmnic{i}"
+        assert vswitch["Name"] == vmnic_vswitch_map.pop(i)
+    assert len(vmnic_vswitch_map) == 0
+
+async def verify_syno(conn, logger):
     print("syno")
 
 DRIVER_MAP = {
@@ -91,16 +185,20 @@ async def check(node, attr):
         prompt = await conn.get_prompt()
         assert node.lower() in prompt.lower()
 
-        # Perform detail checks on a given device using the open connection
-        await VERIFY_MAP[platform](conn)
+        # Perform detailed checks on a given device using conn w/ logging
+        logger = setup_logger(node, f"logs/{node}.txt")
+        await VERIFY_MAP[platform](conn, logger)
 
 
-async def main():
+async def main(config_file):
 
     # Load config details from file
     yaml = ruamel.yaml.YAML()
-    with open("config.yml", "r", encoding="utf-8") as handle:
+    with open(config_file, "r", encoding="utf-8") as handle:
       config = yaml.load(handle)
+
+    for node, attr in config["skip"].items():
+        print(f"Skipping node {node} with attr {attr}")
     
     # Instantiate coroutine into tasks for each node, merging in the login dict
     tasks = [
@@ -112,6 +210,8 @@ async def main():
     task_future = asyncio.gather(*tasks)
     await task_future
 
+    print("OK")
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main("config.yml"))
